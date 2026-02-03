48adbb84f91fa55b72845eaf3a1169fb30032460
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-03 15:13:53 +0200

    answers_6 is done

--- answers.py
+++ answers.py
@@ -1,12 +1,9 @@
 
-def Word_test(file_name , word):
-      counter = 0
-      with open(file_name , "r") as file :
-        for w in file:
-            new_word = w.split()
-            for i in new_word:
-             if i == word:
-              counter += 1
-        return counter     
-test = Word_test("answers.py" , "counter")
-
+def Word_test(file_name , file_wirth):
+  with open(file_name , "r") as file , open(file_wirth , "w") as wir:
+       for line in file :
+         wir.write(line)      
+          
+          
+test = Word_test("answers.py" , "test1.txt")
+print(test)
