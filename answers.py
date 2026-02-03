ee9f5727b35b44bfddb1c64fc49c8f869ad5ae09
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-03 16:31:05 +0200

    answers_8 is done

--- answers.py
+++ answers.py
@@ -1,19 +1,6 @@
 
-def Word_test(file_name, new_file_name, Search, exchange):
-    with open(file_name, "r") as file, open(new_file_name, "w") as new:
-        for line in file:
-            words = line.split()
-            updated_words = [] 
-            
-            for word in words:
-                if word == Search:
-                    updated_words.append(exchange)
-                else:
-                    updated_words.append(word)
-            
-          
-            final_line = " ".join(updated_words) + "\n"
-            new.write(final_line)
+def add_text(file_name, text1, text2):
+    with  open(file_name, "a") as new:
+         new.write(f"\n{text1}:{text2}")
 
-
-Word_test("data.txt", "data_updated.txt", "kodcod", "8200")
+add_text("data.txt" , "city" , "bs")         
