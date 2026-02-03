9e28bd93be0f71916b7ce736b13c700973352e95
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-03 12:15:18 +0200

    answers_3 is done

--- answers.py
+++ answers.py
@@ -1,5 +1,7 @@
-def new_file(file_name , text):
-      with open(file_name , "w") as file:
-         writer =  file.write(text)
-         
-new_file("test1.txt" , "23523" ) 
+def new_file(file_name_source , target_file):
+      with open(file_name_source , "r") as file , open(target_file , "w") as target :
+          
+         for line in file:
+          target.write(line)
+
+new_file("data.txt" , "test1.txt")
